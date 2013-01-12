ff2e30d52404df7c866aa344060fa170e0ac29fc
KilianHett <kilian@octocore.(none)>
2013-01-12 15:01:15 +0100

    Ajout PanelImage

---
+++ src/PanelImage.java
@@ -0,0 +1,53 @@
+
+import javax.swing.JPanel;
+import java.awt.image.BufferdImage;
+import java.awt.Graphics;
+
+/**
+ * @author HETT KILIAN
+ * @see FrameView
+ * Cette classe se charge d'afficher les images
+ * elle supporte les comportements de clique de la souris
+**/
+public class PanelImage extends JPanel
+{
+	private BufferedImage image;
+
+	public PanelImage(String url)
+	{
+		super();
+		loadImage(url);
+	}
+
+	public PanelImage()
+	{
+		this("");
+	}
+
+	/**
+	 * @param String url -- Chemin vers l'image à charger
+	 * Fonction chargeant une image et l'affichant
+	*/
+	public void loadImage(String url)
+	{
+		try
+		{
+			image = ImageIO.read(url);
+		}
+		catch (Exception e)
+		{
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		}	
+	}
+
+	/**
+	  * @param Graphic g
+	  * Surcharge de la méthode paint : permet d'afficher l'image
+	*/
+	public void paint(Graphics g)
+	{
+		g.drawImage(image,0,0,null);
+	}
+
+}
